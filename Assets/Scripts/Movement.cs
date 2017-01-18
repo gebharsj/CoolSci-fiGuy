38a7df94a440450a0153fac0ad5f1498bfc7b0e6
gebharsj <gebharsj@gmatc.matc.edu>
2017-01-18 15:38:12 -0600

    added character + anims, added movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10f;
+    float hori;
+    float vert;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        anim.SetFloat("Horizontal", hori);
+        anim.SetFloat("Vertical", vert);
+
+        transform.Translate(new Vector3(hori * Time.deltaTime * speed, 0f, vert * Time.deltaTime * speed));
+    }
+}

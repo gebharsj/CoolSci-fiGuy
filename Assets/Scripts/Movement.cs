745349b55aa1e9876879f17b9293c65cf3bf4111
Borgeshc <hborges@digitaliris.com>
2017-01-19 22:26:09 -0600

    removed the land and seperated jump into its own layer

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-
+public class Movement : MonoBehaviour
+{
     public float speed = 10f;
     public float jumpForce = 10f;
     float hori;
@@ -19,7 +19,6 @@ public class Movement : MonoBehaviour {
     Animator anim;
     Rigidbody rb;
 
-	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
@@ -27,14 +26,18 @@ public class Movement : MonoBehaviour {
         m_speed = speed;
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
         hori = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
-        anim.SetFloat("Horizontal", hori);
-        anim.SetFloat("Vertical", vert);
 
         CheckGroundedStatus();
+
+        if (isGrounded)
+        {
+            anim.SetFloat("Horizontal", hori);
+            anim.SetFloat("Vertical", vert);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
@@ -75,29 +78,13 @@ public class Movement : MonoBehaviour {
             if ((hitInfo.collider.tag == "Ground" || hitInfo.collider.tag == "Environment") && hitInfo.distance <= groundCheckDistance)
             {
                 isGrounded = true;
-                if (anim.GetCurrentAnimatorStateInfo(0).IsName("jump_down"))
-                    StartCoroutine(SetLandBool());
             }
             else
             {
                 isGrounded = false;
+                anim.SetBool("isJumping", false);
                 anim.SetFloat("Height", hitInfo.distance);
-                if (anim.GetCurrentAnimatorStateInfo(0).IsName("jump_down"))
-                    StartCoroutine(SetLandBool());
             }
-        }
-        else
-            isGrounded = false;
-    }
-
-    IEnumerator SetLandBool()
-    {
-        if(!settingLandBool)
-        {
-            settingLandBool = true;
-            yield return new WaitForSeconds(0.25f);
-            anim.SetBool("isJumping", false);
-            settingLandBool = false;
         }
     }
 }
